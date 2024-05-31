af9cb95a9006614fb2b4a1a7e6dcd8963f1ea2f8
didemcbay <didemcicekk@gmail.com>
2024-05-31 10:51:42 +0300

    Matematiksel Islemler

---
+++ C1_MatematikselIslemler/src/main/java/org/example/MatematikselIslemler.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class MatematikselIslemler {
+    public static void main(String[] args) {
+
+        System.out.println(4*(2+5)/2);
+
+        // sonucu virgüllü yazdırmak istersek.
+        double sonuc=4*(2+5)/3 ;
+        System.out.println(sonuc);
+        
+
+    }
+}
